091d0b35dad329ce216c39424d4403f8ce263a3a
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-11-12 12:33:02 +0900

    animate format year

--- ggvisWorldBank/ui.r
+++ ggvisWorldBank/ui.r
@@ -5,7 +5,8 @@ shinyUI(pageWithSidebar(
   div(),
   sidebarPanel(
     sliderInput("year", "Year", min = min(years), max = max(years),
-                value=1970, step = 1),
+                value=1970, step = 1, format="#",
+                animate=animationOptions(interval=2000, loop=TRUE)),
     uiOutput("ggvis_ui")
   ),
   mainPanel(
