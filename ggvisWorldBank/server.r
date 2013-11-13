205cbc59be6172360d3ff5f705f1bdb03aeeafa3
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-11-13 11:39:37 +0900

    hilite/select country

--- ggvisWorldBank/server.r
+++ ggvisWorldBank/server.r
@@ -1,10 +1,18 @@
+library(shiny)
+library(ggvis)
 data(WorldBank, package="animint")
+years <- split(WorldBank, WorldBank$year)
+fertility.range <- range(WorldBank$fertility.rate, na.rm=TRUE)
+life.range <- range(WorldBank$life.expectancy, na.rm=TRUE)
+pop.breaks <- seq(5e8, 1e9, by=1e8)
+pop.range <- range(WorldBank$population, na.rm=TRUE)
 
 shinyServer(function(input, output, session) {
 
-  # A subset of mtcars
   r_gv <- reactive({
-    ggvis(subset(WorldBank,year == input$year),
+    this.year <- years[[as.character(input$year)]]
+    this.country <- subset(this.year, country==input$country)
+    ggvis(this.year,
           props(x = ~fertility.rate, y = ~life.expectancy,
                 size = ~population),
           mark_symbol())
@@ -14,9 +22,18 @@ shinyServer(function(input, output, session) {
   observe_ggvis(r_gv, "ggvis", session, "svg")
 
   output$ggplot2 <- renderPlot({
-    gg <- ggplot(subset(WorldBank, year==input$year))+
-      geom_point(aes(fertility.rate, life.expectancy, clickSelects=country,
-                     showSelected=year, colour=region, size=population))
+    this.year <- years[[as.character(input$year)]]
+    this.country <- subset(this.year, country==input$country)
+    gg <- ggplot()+
+      geom_point(aes(fertility.rate, life.expectancy,
+                     colour=region, size=population),
+                 data=this.year)+
+      geom_text(aes(fertility.rate, life.expectancy, label=country),
+                data=this.country)+
+      continuous_scale("size","area",palette=function(x){
+        scales:::rescale(sqrt(abs(x)), c(2,10))
+      },breaks=pop.breaks, limits=pop.range)+
+      xlim(fertility.range)+ylim(life.range)
     print(gg)
   })
 
--- ggvisWorldBank/ui.r
+++ ggvisWorldBank/ui.r
@@ -1,12 +1,15 @@
 data(WorldBank, package="animint")
 years <- unique(WorldBank$year)
+countries <- unique(WorldBank$country)
+countryList <- structure(as.list(countries),names=countries)
 
 shinyUI(pageWithSidebar(
   div(),
   sidebarPanel(
     sliderInput("year", "Year", min = min(years), max = max(years),
                 value=1970, step = 1, format="#",
-                animate=animationOptions(interval=2000, loop=TRUE))
+                animate=animationOptions(interval=2000, loop=TRUE)),
+    selectInput("country", "Country", countryList, selected="Japan")
   ),
   mainPanel(
     ggvis_output("ggvis"),
