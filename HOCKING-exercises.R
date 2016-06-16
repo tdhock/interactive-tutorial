1935f34aa66e5c895e2a38767c7c928a0794ca03
Toby Dylan Hocking <tdhock5@gmail.com>
2016-06-16 13:54:04 -0400

    last ex

--- HOCKING-exercises.R
+++ HOCKING-exercises.R
@@ -44,7 +44,8 @@ viz.three.plots <- list(
               data=WorldBank))
 structure(viz.three.plots, class="animint")
 
-## Exercise: add another geom.
+## Exercise: add another geom. Just add some more geoms with
+## aes(showSelected=year) to the data viz.
 years <- data.frame(year=unique(WorldBank$year))
 tails.list <- list()
 WorldBank$other.year <- WorldBank$year
@@ -71,10 +72,85 @@ viz.scatter <- list(
               data=years))
 structure(viz.scatter, class="animint")
 
-## Exercise: add another plot.
+## Exercise: add another plot. Just add another ggplot to the viz list.
 viz.scatter.ts <- viz.scatter
 viz.scatter.ts$tsFert <- ggplot()+
   geom_line(aes(x=year, y=fertility.rate, color=region, group=country),
             data=WorldBank)+
   geom_vline(aes(xintercept=year, showSelected=year), data=years)
 structure(viz.scatter.ts, class="animint")
+
+## Exercise: make an animated data viz that does NOT use smooth
+## transitions. Just add the "time" option to the viz list.
+viz.anim.only <- viz.scatter.ts
+viz.anim.only$time <- list(variable="year", ms=1000)
+structure(viz.anim.only, class="animint")
+
+## Exercise: how to get the geom_text to disappear along with the
+## point when the region legend is clicked? Add
+## aes(showSelected=region).
+viz.click <- list(
+  scatter=ggplot()+
+    geom_point(aes(x=life.expectancy, y=fertility.rate, color=region,
+                   key=country,
+                   showSelected=year,
+                   clickSelects=country),
+               data=WorldBank)+
+    geom_text(aes(x=life.expectancy, y=fertility.rate, label=country,
+                  key=country,
+                  showSelected=year,
+                  showSelected2=country,
+                  showSelected3=region), #added!
+              data=WorldBank),
+  duration=list(year=2000))
+structure(viz.click, class="animint")
+
+## Exercise: how to get the geom_text to disappear when you click it?
+## Add aes(clickSelects=country).
+viz.multiple <- list(
+  scatter=ggplot()+
+    geom_point(aes(x=life.expectancy, y=fertility.rate, color=region,
+                   key=country,
+                   showSelected=year,
+                   clickSelects=country),
+               data=WorldBank)+
+    geom_text(aes(x=life.expectancy, y=fertility.rate, label=country,
+                  key=country,
+                  clickSelects=country, #added!
+                  showSelected=year,
+                  showSelected2=country,
+                  showSelected3=region), 
+              data=WorldBank),
+  first=list(
+    year=1970,
+    country="United States",
+    region=c("North America", "South Asia")),
+  selector.types=list(country="multiple"),
+  duration=list(year=2000))
+structure(viz.multiple, class="animint")
+
+## Exercise: add animation and layers.
+viz.timeSeries <- viz.multiple
+viz.timeSeries$timeSeries <- ggplot()+
+  geom_tallrect(aes(xmin=year-0.5, xmax=year+0.5,
+                    clickSelects=year),
+                alpha=0.5,
+                data=years)+
+  geom_line(aes(x=year, y=fertility.rate, group=country, color=region,
+                clickSelects=country),
+            size=3,
+            alpha=0.6,
+            data=WorldBank)
+viz.timeSeries$time <- list(variable="year", ms=1000)
+viz.timeSeries$scatter <- viz.multiple$scatter+
+    geom_path(aes(x=life.expectancy, y=fertility.rate, color=region,
+                  group=country,
+                  key=country,
+                  clickSelects=year,
+                  showSelected=year),
+              data=tails)+
+    geom_text(aes(55, 9, label=paste("year =", year),
+                  showSelected=year),
+              data=years)
+
+structure(viz.timeSeries, class="animint")
