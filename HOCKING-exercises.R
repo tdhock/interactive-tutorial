13f59128fd063757da32e7a66b3674f7c0d9443e
Toby Dylan Hocking <tdhock5@gmail.com>
2016-06-16 13:38:28 -0400

    add layers and plot

--- HOCKING-exercises.R
+++ HOCKING-exercises.R
@@ -6,7 +6,7 @@ WorldBank1975 <- subset(WorldBank, year==1975)
 ## making a new animint. Quantitative variables like population are
 ## best shown using the x/y axes or point size. Qualitative variables
 ## like lending are best shown using point color or fill.
-scatter.viz <- list(
+viz.1975 <- list(
   scatter=ggplot()+
   geom_point(
     mapping=aes(
@@ -16,10 +16,11 @@ scatter.viz <- list(
       size=population),
     data=WorldBank1975)+
   scale_size_animint(breaks=10^(9:5)))
-structure(scatter.viz, class="animint")
+structure(viz.1975, class="animint")
 
 ## Exercise: use animint to create a data viz with three plots, by
 ## creating a list with three ggplots. 
+WorldBankBefore1975 <- subset(WorldBank, 1970 <= year & year <= 1975)
 viz.three.plots <- list(
   scatter=ggplot()+
     geom_point(
@@ -44,5 +45,36 @@ viz.three.plots <- list(
 structure(viz.three.plots, class="animint")
 
 ## Exercise: add another geom.
+years <- data.frame(year=unique(WorldBank$year))
+tails.list <- list()
+WorldBank$other.year <- WorldBank$year
+for(year.value in years$year){
+  ## For every year, compute the tail (geom_path) which is the data
+  ## for the previous 5 years.
+  one.tail <- subset(
+    WorldBank, year.value-5 <= other.year & other.year <= year.value)
+  one.tail$year <- year.value
+  tails.list[[paste(year.value)]] <- one.tail
+}
+tails <- do.call(rbind, tails.list)
+viz.scatter <- list(
+  scatter=ggplot()+
+    geom_path(aes(x=life.expectancy, y=fertility.rate, color=region,
+                  group=country,
+                  showSelected=year),
+              data=tails)+
+    geom_point(aes(x=life.expectancy, y=fertility.rate, color=region,
+                   showSelected=year),
+               data=WorldBank)+
+    geom_text(aes(55, 9, label=paste("year =", year),
+                  showSelected=year),
+              data=years))
+structure(viz.scatter, class="animint")
 
 ## Exercise: add another plot.
+viz.scatter.ts <- viz.scatter
+viz.scatter.ts$tsFert <- ggplot()+
+  geom_line(aes(x=year, y=fertility.rate, color=region, group=country),
+            data=WorldBank)+
+  geom_vline(aes(xintercept=year, showSelected=year), data=years)
+structure(viz.scatter.ts, class="animint")
